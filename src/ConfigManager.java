98c5ad56bedd6a204671ddf030c3d70630def070
Benjamin Russell <benrr101@csh.rit.edu>
2013-01-29 23:40:41 -0500

    Some preliminary work on the configuration manager

--- src/ConfigManager.java
+++ src/ConfigManager.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 /**
  * The configuration manager. It loads the site configuration files for each of
  * the sites supported by the server. This class follows the
@@ -10,14 +12,34 @@ public class ConfigManager {
     // MEMBER VARIABLES ////////////////////////////////////////////////////
     private static ConfigManager ourInstance = new ConfigManager();
 
+    private File configFolder;
+
     // CONSTRUCTOR /////////////////////////////////////////////////////////
+    private ConfigManager() {
+        // Open the folder of configurations
+        configFolder = new File(siteConfigFolder);
+        if(!configFolder.exists()) {
+            throw new ConfigurationException("Site configurations folder does not exist");
+        }
+        if(!configFolder.isDirectory()) {
+            throw new ConfigurationException("Site configurations folder is not a directory");
+        }
+    }
+
+    // METHODS /////////////////////////////////////////////////////////////
     public static ConfigManager getInstance() {
         return ourInstance;
     }
 
-    // METHODS /////////////////////////////////////////////////////////////
-    private ConfigManager() {
-
+    public void getSiteConfigurations() {
+        // Load the files in the configuration
+        File[] siteConfigurationFiles = configFolder.listFiles();
     }
 
+}
+
+public class ConfigurationException extends RuntimeException {
+    ConfigurationException(String message) {
+        super(message);
+    }
 }
---
+++ src/SiteConfiguration.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Omega
+ * Date: 1/29/13
+ * Time: 11:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SiteConfiguration {
+}
